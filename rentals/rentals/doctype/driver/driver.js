d674fafe5a56552c06512b1888e9d3f328d16edb
James Titus <titusjames299@gmail.com>
2024-07-09 05:21:20 +0000

    initial commit -- Driver doctype

---
+++ rentals/rentals/doctype/driver/driver.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, irfan-cabs and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Driver", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ rentals/rentals/doctype/driver/test_driver.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, irfan-cabs and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDriver(FrappeTestCase):
+	pass
